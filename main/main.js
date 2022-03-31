83a72504b50559598920164a64a8f7eba9f39f9e
Ricky Johnson <rickyjohnson@Rickys-MBP.lan>
2022-03-31 15:09:52 -0400

    Final MVP

--- main/main.js
+++ main/main.js
@@ -1,22 +1,92 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  let articleDisplay = 'random';
+
+  let newButton = document.createElement('Button');
+  newButton.setAttribute('class','randomize-button');
+  newButton.innerText = 'Randomize';
+  newButton.addEventListener('click', ()=> {
+    
+    let allsStories = document.querySelectorAll('.story-container');
+    allsStories.forEach(story=>{
+      story.remove();
+    })
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json?')
+    .then((data) => data.json())
+    .then((data)=> getStorydata(data, articleDisplay))
+    .catch(error => console.log(`Im the error ${error}`));   
+  })
+  document.body.appendChild(newButton);
+  // newLink.setAttribute('href', data.url);
+  // newLink.setAttribute('target', "_blank");
+  // newLink.innerText = data.title;
  
 // code to fetch top stories data from Hackernews api
   fetch('https://hacker-news.firebaseio.com/v0/topstories.json?')
     .then((data) => data.json())
-    .then((data)=> getStorydata(data))
+    .then((data)=> getStorydata(data, 'top'))
     .catch(error => console.log(`Im the error ${error}`));
 
-//Showing top 10 sories from the data
-  function getStorydata(storyArray) {
+//Showing top 10 stories from the data
+  function getStorydata(storyArray, sequence='top') {
     // Iteration through the first 10 elements of the the JSON data
-    for (let index = 0; index < 10; index++) {
-      const story = storyArray[index];
-      fetch(`https://hacker-news.firebaseio.com/v0/item/${story}.json?`)
-      .then((data) => data.json())
-      .then((data) => populateNews(data, index))
-      // .then((data) => console.log(data))
-      .catch(error => console.log(`Im the error ${error}`));
+
+    if (sequence === 'top') {
+      
+      let promises = [];
+      for (let index = 0; index < 10; index++) {
+
+        //Working Code
+        const story = storyArray[index];
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${story}.json?`)
+        .then((data) => data.json())
+        .then((data) => populateNews(data, index))
+        // .then((data) => console.log(data))
+        .catch(error => console.log(`Im the error ${error}`));
+        document.querySelector('#header').innerText = 'Hacker News Top 10 Articles';
+        newButton.innerText = 'Randomize';
+        articleDisplay = 'random';
+
+        //Experimenting Code to Keep Sequence
+        // const story = storyArray[index];
+        // promises.push(fetch(`https://hacker-news.firebaseio.com/v0/item/${story}.json?`)
+        // .then((data) => data.json())
+        // .then((data) => populateNews(data, index))
+        // // .then((data) => console.log(data))
+        // .catch(error => console.log(`Im the error ${error}`)));
+
+
+      }
+      // console.log(promises);
+      // Promise.all(promises);
+      document.querySelector('#header').innerText = 'Hacker News Top 10 Articles';
+      newButton.innerText = 'Randomize';
+      articleDisplay = 'random';
+    } else { //Randomized 10
+
+      let randomNum; 
+
+      for (let index = 0; index < 10; index++) {
+        console.log(randomNum);
+        randomNum = Math.floor(Math.random() * 500);
+
+        // console.log(typeof storyArray);
+        // console.log(typeof storyArray);
+        const story = storyArray[randomNum];
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${story}.json?`)
+        .then((data) => data.json())
+        .then((data) => populateNews(data, index))
+        // .then((data) => console.log(data))
+        .catch(error => console.log(`Im the error ${error}`));
+      }
+
+      // console.log(document.querySelector('header'));
+      document.querySelector('#header').innerText = 'Randomized 10 Articles';
+      newButton.innerText = 'Top 10';
+      articleDisplay = 'top';
     }
+
+
 
   // Where we keep the entirety of the DOM
   function populateNews(data, index) {
@@ -28,6 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let newLink = document.createElement('a');
     newLink.setAttribute('href', data.url);
+    newLink.setAttribute('target', "_blank");
     newLink.innerText = data.title;
 
     let newButton = document.createElement('button');
@@ -41,7 +112,8 @@ document.addEventListener('DOMContentLoaded', () => {
     
     let newThreadLink = document.createElement('a');
     newThreadLink.setAttribute('href', storyThread);
-    newThreadLink.innerText = 'HN Discussion Link';
+    newThreadLink.setAttribute('target', "_blank");
+    newThreadLink.innerText = 'Article Thread Discussion';
 
     newParagraph.innerText = `Article Score: ${data.score} \n Comments: ${data.descendants}`;
 
@@ -50,6 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
     newDiv.appendChild(newButton);
     newDiv.appendChild(newParagraph);
     newDiv.appendChild(newThreadLink);
+
    
     return data;    
   }
